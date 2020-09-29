693f99204ea6c80056b6bc97ff5092bde4896744
LachelleF <lachellefryett@gmail.com>
2020-09-29 14:17:01 -0400

    syncing proxy test file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-
+//following notes per research
 export let options = {
 
   stages: [
@@ -10,19 +10,24 @@ export let options = {
     { duration: '3m', target: 10 },
     { duration: '1m', target: 100 }, // around breaking point of 200 users / second
     { duration: '3m', target: 100 },
-    // { duration: '1m', target: 1000 }, // beyond the breaking point
-    // { duration: '3m', target: 1000 },
-    { duration: '1m', target: 0 }   // scale down. Recovery stage.
+    { duration: '1m', target: 1000 }, // beyond the breaking point
+    { duration: '3m', target: 1000 },
+    { duration: '1m', target: 100 }, // scale down. Recovery stage.
+    { duration: '3m', target: 100 },
+    { duration: '1m', target: 10 },
+    { duration: '3m', target: 10 },
+    { duration: '1m', target: 1 },
+    { duration: '3m', target: 1 },
   ],
 
 }
 export default function () {
-  let randomItemNum = Math.ceil(Math.random() * 10000000);
-  let res = http.get(`http://127.0.0.1:3010/${randomItemNum}`);
+  let randomItemNum = Math.ceil(Math.random() * 3000000);
+  let res = http.get(`http://127.0.0.1:3010/product?itemID=${randomItemNum}`);
   check(res,
     {
       'status was 201': r => r.status == 201,
-      // 'transaction time OK': r => r.timings.duration < 500
     });
   sleep(1);
 }
+
